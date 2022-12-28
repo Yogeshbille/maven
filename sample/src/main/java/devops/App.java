8dd30c84cfe07f18a6d40738fe71cec4fef7cab0
EC2 Default User <ec2-user@ip-172-31-10-183.ap-south-1.compute.internal>
2022-12-28 15:57:23 +0000

    code correction

--- sample/src/main/java/devops/App.java
+++ sample/src/main/java/devops/App.java
@@ -4,14 +4,18 @@ package devops;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
-        System.out.println( "Hello World! this is an output from server" );
+<<<<<<<D
+        System.out.println( "Hello World! this is an output from server and this is latest code" );
 =======
-        System.out.println( "Hello World! I am testing this code to show with jenkins job with auto trigger, another check" );
+>>>>>>> b8ec3406734993c0d6520034b66a5034d33965f6
+    }
+}
+ );
+=======
 >>>>>>> b8ec3406734993c0d6520034b66a5034d33965f6
     }
 }
