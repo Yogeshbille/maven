20ef485a276a869961db5c74af5c5ae1f4bf7e8d
EC2 Default User <ec2-user@ip-172-31-10-183.ap-south-1.compute.internal>
2022-12-28 16:01:28 +0000

    code correction

--- sample/src/main/java/devops/App.java
+++ sample/src/main/java/devops/App.java
@@ -4,18 +4,10 @@ package devops;
  * Hello world!
  *
  */
-public class App
+public class App 
 {
     public static void main( String[] args )
     {
-<<<<<<<D
-        System.out.println( "Hello World! this is an output from server and this is latest code" );
-=======
->>>>>>> b8ec3406734993c0d6520034b66a5034d33965f6
-    }
-}
- );
-=======
->>>>>>> b8ec3406734993c0d6520034b66a5034d33965f6
+        System.out.println( "Hello World! This is the correction of the code" );
     }
 }
