e5253cdf29f59e2aa836b6511469febb667cf05a
Yogeshbille <116893877+Yogeshbille@users.noreply.github.com>
2022-12-28 14:00:26 +0530

    second time

--- sample/src/main/java/devops/App.java
+++ sample/src/main/java/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! I am testing this code to show with jenkins job with auto trigger" );
+        System.out.println( "Hello World! I am testing this code to show with jenkins job with auto trigger,second check" );
     }
 }
