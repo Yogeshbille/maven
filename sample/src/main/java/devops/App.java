be0fa6b6162998edaacfbe6d3cf7c11665af0a5e
Yogeshbille <116893877+Yogeshbille@users.noreply.github.com>
2022-12-28 13:23:24 +0530

    webhook trigger

--- sample/src/main/java/devops/App.java
+++ sample/src/main/java/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! I am testing this code to show with jenkins job with auto trigger do" );
+        System.out.println( "Hello World! I am testing this code to show with jenkins job with auto trigger" );
     }
 }
